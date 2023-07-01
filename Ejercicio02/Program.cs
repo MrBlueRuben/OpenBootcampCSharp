3719ca2698ee8a06ce87df18c7cf8378fd958bfc
Ruben Mrblue <sanchez.alvarez.ruben@gmail.com>
2023-07-01 18:14:40 +0200

    ejercicio 1 leccion 2 hecho

--- Ejercicio02/Program.cs
+++ Ejercicio02/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
+
+            /* Ejercicio 01
+            Variables Escribe un programa que reciba datos de una persona y genera un mensaje, 
+            usa una variable para cada dato y otra para el mensaje. 
+            Ej: nombre, apellido, edad, sabe programar, etc.
+            */
+            Console.WriteLine("Escribe tu nombre:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Escribe tu Apellido:");
+            string surname = Console.ReadLine();
             Console.WriteLine("Hello, World!");
+            string edad = Console.ReadLine();
+            Console.WriteLine("¿Sabes programar? si/no");
+            string progLang = Console.ReadLine();
+            Console.WriteLine("Hola soy, "+ name +" "+surname+", tengo "+edad+" años"+" y "+progLang + " ,se programar " );
         }
     }
 }
