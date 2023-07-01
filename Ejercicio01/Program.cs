98aad4ec399acfa3c08ebdc98e25879bc4a634e7
Ruben Mrblue <sanchez.alvarez.ruben@gmail.com>
2023-07-01 15:57:06 +0200

    Add project files.

---
+++ Ejercicio01/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Ejercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce tu nombre");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello," + name);
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
