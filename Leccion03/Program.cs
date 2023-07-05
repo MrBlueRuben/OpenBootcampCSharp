c42657c4acc15506eea711fa984c6ae74e06d853
Ruben Mrblue <sanchez.alvarez.ruben@gmail.com>
2023-07-05 10:27:26 +0200

    not working learnin how to write structs

---
+++ Leccion03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Channels;
+
+namespace Leccion03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+        /*Ejercicio 1
+            Crea una estructura de datos para un cliente con estos campos:
+            Nombre completo
+            Teléfono
+            Dirección
+            Email
+            Si es nuevo cliente
+
+            Bonus: escribe un método para presentar estos datos desde la estructura al hacer Console.WriteLine(...)*/
+        public struct Empleado
+        {
+            public string nombreCompleto = "Paco Pérez García";
+            public string Telefono = "+34123654489";
+            public string Direccion = "C/Rue del Percebe, 13";
+            public string eMail = "paquitochoc@openbootcam.com";
+            public bool nuevoCliente = true;
+
+            public Empleado()
+            {
+                if (nuevoCliente == true)
+                {
+                    Console.WriteLine($"Ud. es: {nombreCompleto} con tel: {Telefono} y dirección {Direccion}.\n Gracias por ser nuestro cliente");
+                }
+                else
+                {
+                    Console.WriteLine("Ud. no es cliente nuestro");
+                }
+
+            }
+
+        }
+       
+    }
+}
